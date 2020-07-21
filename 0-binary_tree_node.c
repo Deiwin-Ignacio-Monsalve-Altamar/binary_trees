739bd960a56811b5820292cbe6382de06c840e8d
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-07-21 11:05:46 -0500

    fix function that creates a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,15 +9,10 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = value;
-
-	new_node->parent = (binary_tree_t *)malloc(sizeof(binary_tree_t *));
-	if (!new_node->parent)
-		return (NULL);
 	new_node->parent = parent;
 
 	return (new_node);
