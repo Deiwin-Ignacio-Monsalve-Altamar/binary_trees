82530f8abbc646998076dd514d3ad7c279a62aa5
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-07-21 13:37:20 -0500

    function that checks if a node is preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -13,10 +13,10 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	func(tree->n);
-	if (tree->left == NULL)
+	if (tree->left != NULL)
 	{
 		binary_tree_preorder(tree->left, func);
 	}
-	if (tree->right == NULL)
+	if (tree->right != NULL)
 		binary_tree_preorder(tree->right, func);
 }
